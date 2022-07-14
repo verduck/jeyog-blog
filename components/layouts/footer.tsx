c3ea804b8b29360b643d3dafc9a20754927e702b
jeyog <choijy8816@gmail.com>
2022-07-14 21:59:25 +0900

    divide footer

---
+++ components/layouts/footer.tsx
@@ -0,0 +1,29 @@
+
+import { Box } from '@mui/material'
+import Image from 'next/image'
+
+export default function Footer() {
+    return (
+        <>
+            <Box sx={{
+                display: 'flex',
+                flex: 1,
+                padding: '2rem 0',
+                borderTop: '1px solid #eaeaea',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }} component="footer">
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <span>
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    </span>
+                </a>
+            </Box>
+        </>
+    )
+}
