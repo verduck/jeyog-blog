3d722633cce475449d8a7b2dd3b44bee1e0005a5
jeyog <choijy8816@gmail.com>
2022-07-18 19:56:00 +0900

    change env variable name SECRET to NEXTAUTH_SECRET

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,12 +2,13 @@ import NextAuth, { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
-    secret: process.env.SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
             profile(profile) {
+                console.log(profile)
                 return {
                     id: profile.id,
                     name: profile.name,
