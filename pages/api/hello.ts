dcd416e36b45960a98c3ba3d43a7d76c046467f7
jeyog <choijy8816@gmail.com>
2022-07-20 21:35:51 +0900

    change response

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from './auth/[...nextauth]'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const session = await unstable_getServerSession(req, res, authOptions)
+  if (!session) {
+    res.status(401).json({ name: 'unauthorized' })
+    return
+  }
+  res.status(200).json({ name: session?.user?.name! })
 }
