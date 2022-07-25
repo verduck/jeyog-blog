294d25bb758a5ef9fd05d33043496ee7a0f8fd36
jeyog <choijy8816@gmail.com>
2022-07-25 21:28:21 +0900

    change name source

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,20 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { unstable_getServerSession } from 'next-auth'
-import { authOptions } from './auth/[...nextauth]'
+import { getToken } from 'next-auth/jwt'
+import { getSession } from 'next-auth/react'
 
 type Data = {
   name: string
 }
 
+const secret = process.env.NEXTAUTH_SECRET
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const session = await unstable_getServerSession(req, res, authOptions)
-  if (!session) {
-    res.status(401).json({ name: 'unauthorized' })
-    return
-  }
-  res.status(200).json({ name: session?.user?.name! })
+  const token = await getToken({ req, secret })
+  res.status(200).json({ name: token?.name as string })
 }
