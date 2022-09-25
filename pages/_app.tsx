3203ad07a6ae8b53b9d7ebb3fa1228f745fe6d48
jeyog <choijy8816@gmail.com>
2022-09-25 23:28:39 +0900

    wrap app component with QueryClientProvider and Hydrate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,13 @@ import { SessionProvider  } from 'next-auth/react'
 import Layout from '../components/layouts/layout'
 import { createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient())
   const [mode, setMode] = React.useState<'light' | 'dark'>('light')
   const colorMode = React.useMemo(
     () => ({
@@ -30,13 +33,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <SessionProvider session={pageProps.session}>
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </ThemeProvider>
+          </ColorModeContext.Provider>
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   )
 }
