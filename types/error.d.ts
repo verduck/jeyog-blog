dc925beb0dc870409290c66d579cd7d928cb9e14
jeyog <choijy8816@gmail.com>
2022-09-25 23:34:08 +0900

    add error type

---
+++ types/error.d.ts
@@ -0,0 +1,27 @@
+interface CustomError extends Error {
+    code: Number
+}
+
+interface CustomErrorClass {
+    new (message: string, ...args: any[]): CustomError
+}
+
+class NotFoundError implements CustomError {
+    code: 404
+    name: 'Not Found Error'
+    message: string
+    
+    constructor(message: string) {
+        this.message = message
+    }
+}
+
+class UnauthorizedError implements CustomError {
+    code: 401
+    name: 'Unauthorized Error'
+    message: string
+
+    constructor(message: string) {
+        this.message = message
+    }
+}
