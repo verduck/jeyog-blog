fbb902c09f5ecc34d9abb1c8abbdd54cf49a86f2
jeyog <choijy8816@gmail.com>
2022-09-25 19:36:22 +0900

    add hook useAuth to check admin

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,15 @@
+import { useSession } from "next-auth/react";
+
+function useAuth() {
+    const { data: session } = useSession()
+
+    const isAdmin = () => {
+        return session?.user.id === 80824142
+    }
+
+    return {
+        isAdmin
+    }
+}
+
+export default useAuth
