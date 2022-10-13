139df58b85eb29e76195478b2248f3cced418c99
jeyog <choijy8816@gmail.com>
2022-10-13 21:33:16 +0900

    add api get about by githubid

--- pages/api/abouts/[githubId].tsx
+++ pages/api/abouts/[githubId].tsx
@@ -1,13 +1,14 @@
+import { findAboutByGithubId } from '@models/abouts'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
 
 const handler = nc<NextApiRequest, NextApiResponse>({
   onError: (err, req, res, next) => {},
   onNoMatch: (req, res) => {},
-}).get((req, res) => {
-  res.status(200).json({
-    message: 'ok',
-  })
+}).get(async (req, res) => {
+  const { githubId } = req.query
+  const about = await findAboutByGithubId(Number(githubId))
+  res.status(200).json(about)
 })
 
 export default handler
